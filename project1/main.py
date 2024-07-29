61a0dcd96202e919b5746592ea755088c5de96aa
Vatsal Agarwal <vatsal08agarwal@gmail.com>
2024-07-30 03:30:36 +0530

    club/band name generator

---
+++ project1/main.py
@@ -0,0 +1,4 @@
+print("Need a suggestion for the name of your band?")
+abstract = input("What's the emotion that is going in your mind?\n")
+pet = input("What's your Favourite animal's name?\n")
+print(f"Your band name could be {abstract} {pet}")
